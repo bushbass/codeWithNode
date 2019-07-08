db0c9dedc67c2cbec89af4a76aa6d1378ccd147c
bushbass <alex.nielsen@gmail.com>
2019-07-08 15:58:46 -0400

    add post routes

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,53 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
+/*  GET posts index
+    /posts
+*/
 router.get('/', (req, res, next)=> {
-  res.render('index', { title: 'Surf Shop - Home' });
+  res.send('from /posts')
+});
+
+/*  GET create posts form
+    /posts
+*/
+router.get('/new', (req, res, next)=> {
+  res.send('from /posts/new')
+});
+
+/*  POST create post action
+    /posts
+*/
+router.post('/', (req, res, next)=> {
+  res.send('from create action /posts')
+});
+
+/*  GET one post
+    /posts/:id
+*/
+router.get('/:id', (req, res, next)=> {
+  res.send('from get one post /posts/:id')
+});
+
+/*  GET edit post form
+    /posts/:id
+*/
+router.get('/:id/edit', (req, res, next)=> {
+  res.send('from edit form /posts/:id')
+});
+
+/*  PUT edit post action
+    /posts/:id
+*/
+router.put('/:id', (req, res, next)=> {
+  res.send('from edit put /posts/:id')
+});
+
+/*  DELETE one post
+    /posts/:id
+*/
+router.delete('/:id', (req, res, next)=> {
+  res.send('from delete /posts/:id')
 });
 
 module.exports = router;
